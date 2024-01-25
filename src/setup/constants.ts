66c012fc934ca020826211de8ca928f4e75922e1
leo <townhelpemail@gmail.com>
2024-01-24 23:05:35 -0500

    Update constants.ts

--- src/setup/constants.ts
+++ src/setup/constants.ts
@@ -1,6 +1,6 @@
 export const APP_VERSION = import.meta.env.PACKAGE_VERSION;
-export const DISCORD_LINK = "https://discord.movie-web.app";
+export const DISCORD_LINK = "https://discord.com/404";
 export const GITHUB_LINK = "https://github.com/movie-web/movie-web";
-export const DONATION_LINK = "https://ko-fi.com/movieweb";
+export const DONATION_LINK = "https://starnode.host";
 export const GA_ID = "G-44YVXRL61C";
 export const BACKEND_URL = "https://backend.movie-web.app";
